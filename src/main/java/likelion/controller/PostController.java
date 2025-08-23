5b9e7bea60f9317fd7b429f76f10ab8fc69b6871
kyoohyunlee <khyhlee@naver.com>
2025-08-23 16:14:06 +0900

    feat: swagger에서 테스트 되도록 수정

--- src/main/java/likelion/controller/PostController.java
+++ src/main/java/likelion/controller/PostController.java
@@ -1,5 +1,7 @@
 package likelion.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import likelion.dto.PostCreateRequestDto;
 import likelion.dto.PostResponseDto;
 import likelion.service.PostService;
@@ -18,13 +20,16 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/post")
+@Tag(name = "Post", description = "게시글/dto에 String으로 되어있는데 -> Json파일 넣어서 테스트해주세요")
 public class PostController {
 
     private final PostService postService;
+    private final ObjectMapper objectMapper;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> create(@RequestPart("dto") PostCreateRequestDto dto,
+    public ResponseEntity<?> create(@RequestPart("dto") String dtoString,
                                   @RequestPart(value = "image", required = false) MultipartFile image) throws IOException {
+        PostCreateRequestDto dto = objectMapper.readValue(dtoString, PostCreateRequestDto.class);
         Long id = postService.createPost(dto, image);
         return ResponseEntity.created(URI.create("/api/posts/" + id))
                 .body(Map.of("id", id));
